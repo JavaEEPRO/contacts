92b993fba2afef8b1cc0b635f31be5543ddd0632
JavaEEPRO <j2eengineer@gmail.com>
2019-11-20 15:12:59 +0200

    added CustomAuthenticationProvider

---
+++ src/main/java/si/inspirited/security/CustomAuthenticationProvider.java
@@ -0,0 +1,40 @@
+package si.inspirited.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import si.inspirited.persistence.model.User;
+import si.inspirited.persistence.repositories.UserRepository;
+
+public class CustomAuthenticationProvider extends DaoAuthenticationProvider {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Authentication authenticate(Authentication auth) throws AuthenticationException {
+        final User user = userRepository.findByLogin(auth.getName());
+        if ((user == null)) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        final Authentication result = super.authenticate(auth);
+        return new UsernamePasswordAuthenticationToken(user, result.getCredentials(), result.getAuthorities());
+    }
+
+    private boolean isValidLong(String code) {
+        try {
+            Long.parseLong(code);
+        } catch (final NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
