bfde4c237543b1a597cd4929ba64e35184d1e423
JavaEEPRO <j2eengineer@gmail.com>
2019-11-20 10:42:36 +0200

    refactoring: added missing annotations @RepositoryRestResource

--- src/main/java/si/inspirited/persistence/repositories/ContactRepository.java
+++ src/main/java/si/inspirited/persistence/repositories/ContactRepository.java
@@ -1,7 +1,9 @@
 package si.inspirited.persistence.repositories;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import si.inspirited.persistence.model.Contact;
 
+@RepositoryRestResource(path = "contacts", collectionResourceRel = "contacts")
 public interface ContactRepository extends PagingAndSortingRepository<Contact, Long> {
 }
--- src/main/java/si/inspirited/persistence/repositories/UserRepository.java
+++ src/main/java/si/inspirited/persistence/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package si.inspirited.persistence.repositories;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import si.inspirited.persistence.model.User;
 
+@RepositoryRestResource(path = "users", collectionResourceRel = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 }
