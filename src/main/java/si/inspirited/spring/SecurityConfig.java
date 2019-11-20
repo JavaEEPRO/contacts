03707290aeac5c87918dad58aabbd26db1b85508
JavaEEPRO <j2eengineer@gmail.com>
2019-11-20 16:28:17 +0200

    added SecurityConfig and pre-defined configuration; registered following beans into context

---
+++ src/main/java/si/inspirited/spring/SecurityConfig.java
@@ -0,0 +1,143 @@
+package si.inspirited.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
+import si.inspirited.persistence.repositories.UserRepository;
+import si.inspirited.security.*;
+
+@Configuration
+@ComponentScan(basePackages = { "si.inspirited.security" })
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationSuccessHandler myAuthenticationSuccessHandler;
+
+    @Autowired
+    private LogoutSuccessHandler myLogoutSuccessHandler;
+
+    @Autowired
+    private AuthenticationFailureHandler authenticationFailureHandler;
+
+    @Autowired
+    private CustomWebAuthenticationDetailsSource authenticationDetailsSource;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public SecurityConfig() {
+        super();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authProvider());
+    }
+
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+
+                .antMatchers("/users", "/login" , "/status").permitAll()//hasRole("USER")
+                .antMatchers( "/contacts", "/groups" , "/group/{groupId}/contacts" , "/groups/{groupId}").hasAuthority("USER_PRIVILEGE")
+                .antMatchers("/groups").hasAuthority("EXPLORE_GROUPS_PRIVILEGE")
+
+                .anyRequest().access("isAnonymous()")
+
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/status")
+                .failureUrl("/login?error=true")
+
+                .successHandler(myAuthenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler)
+                .authenticationDetailsSource(authenticationDetailsSource)
+                .permitAll()
+
+                .and()
+                .sessionManagement()
+                .invalidSessionUrl("/invalidSess")
+                .maximumSessions(1).sessionRegistry(sessionRegistry()).and()
+                .sessionFixation().none()
+                .and()
+                .logout()
+                .logoutSuccessHandler(myLogoutSuccessHandler)
+                .invalidateHttpSession(false)
+                .logoutSuccessUrl("/logout")
+                .deleteCookies("JSESSIONID")
+                .permitAll()
+
+                .and()
+                .rememberMe().rememberMeServices(rememberMeServices()).key("theKey");
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        final CustomAuthenticationProvider authProvider = new CustomAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Bean
+    public RememberMeServices rememberMeServices() {
+        CustomRememberMeService rememberMeService = new CustomRememberMeService("theKey", userDetailsService, new InMemoryTokenRepositoryImpl());
+        return rememberMeService;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public CustomLoginAuthenticationSuccessHandler customLoginAuthenticationSuccessHandler() {
+        return new CustomLoginAuthenticationSuccessHandler();
+    }
+}
