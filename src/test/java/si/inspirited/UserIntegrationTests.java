eaae71dedde4847424556bc3abf99a9e61e8076f
JavaEEPRO <j2eengineer@gmail.com>
2019-11-26 13:30:20 +0200

    3-rd test falls ( UserIntegrationTests::checkNewAddedUsersForReceivingRolesOnRegistration_whenFirstAddedUserIsAdmin_andNextAreRegularUsers_thenCorrect ) expecting, that first user, added to the system, should automatically have admin role, and any next - user role

--- src/test/java/si/inspirited/UserIntegrationTests.java
+++ src/test/java/si/inspirited/UserIntegrationTests.java
@@ -9,8 +9,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import si.inspirited.persistence.model.Role;
 import si.inspirited.persistence.model.User;
 import si.inspirited.persistence.repositories.UserRepository;
+
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -77,7 +81,50 @@ public class UserIntegrationTests {
         assertEquals(sizeOfStorageAfterFirstUserPosted, sizeOfStorageAfterSecondUserPosted);
     }
 
+    @Test
+    public void checkNewAddedUsersForReceivingRolesOnRegistration_whenFirstAddedUserIsAdmin_andNextAreRegularUsers_thenCorrect() {
+        String mockAdminName = "Admin";
+        String adminStub = getUserStub(mockAdminName);
+        try {
+            mockMvc.perform(post(URL_USERS).content(adminStub));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        User returnedAdmin = userRepository.findByLogin(mockAdminName);
+        assertNotNull(returnedAdmin.getRoles());
+        boolean isAdmin = false;
+        for (Role role : returnedAdmin.getRoles()) {
+            if ("ROLE_ADMIN".equals(role.getAuthority())) { isAdmin = true; }
+        }
+        assertTrue(isAdmin);
+
+        registerCoupleRegularUsers("RegularUser");
+        Iterable<User> allRegistered = userRepository.findAll();
+        for (User user : allRegistered) {
+            if (!mockAdminName.equals(user.getLogin())) {
+                boolean isUser = false;
+                for (Role role : user.getRoles()) {
+                    if ("USER_ROLE".equals(role.getAuthority())) {
+                        isUser = true;
+                    }
+                    assertTrue(isUser);
+                }
+
+            }
+        }
+    }
+
     private String getUserStub(String login) {
         return "{\"login\":\"" + login + "\",\"password\":\"password\"}";
+    }
+
+    private void registerCoupleRegularUsers(String prefixOfLogin) {
+        for (int i = 0; i < 12; i++) {
+            try {
+                mockMvc.perform(post(URL_USERS).content(getUserStub(prefixOfLogin + i)));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
